f54a9048f18cc25d5f93fcf69a728499e6d19d10
Fenjlfat <latypovfanil@gmail.com>
2025-07-19 17:19:03 +0500

    task number 3

---
+++ task_pointer_3.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main()
+{
+	int *iPtr; //pointer to value type int
+	double *dPtr; //pointer to value type double
+	
+	int* iPtr3; //correct synatxis
+	int * iPtr4; //correct syntaxis
+	
+	int *iPtr5, *iPtr6; //2 pointer for value type int
+	std::cout << "well done" << std::endl;
+	return 0;
+}
